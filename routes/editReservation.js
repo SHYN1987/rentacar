0be631782aa3ecf90b1fbdf71eddbbe935857e1a
SHYN1987 <47955936+SHYN1987@users.noreply.github.com>
2021-10-14 20:22:33 +0200

    Update editReservation.js

--- routes/editReservation.js
+++ routes/editReservation.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Reservation = require('../database/reservationdata')
 
-router.get('/edit/:id',(req, res) =>{
+router.get('/editReservation/:id', checkAuthenticated, isAdmin,(req, res) =>{
     var id = req.params.id;
 
 
@@ -11,9 +11,9 @@ router.get('/edit/:id',(req, res) =>{
                   if(!data){
                       res.status(404).send({ message : "Not found user with id "+ id})
                   }else{
-                    res.render("edit", {
+                    res.render("reservationedit", {
                         reservation_id: id,
-                        car: data
+                        reservation: data
                     });
                   }
               })
@@ -22,11 +22,19 @@ router.get('/edit/:id',(req, res) =>{
               })
   })
 
-  router.post('/edit/:id', (req, res)=>{
-    const car = new Reservation({
-        car: req.body.car,
-        startdate: req.body.startdate,
-        enddate: req.body.enddate
+  router.post('/editReservation/:id', (req, res)=>{
+    const reservation = new Reservation({
+      _id: req.params.id,
+      startDate: req.body.startDate,
+      endDate: req.body.endDate,
+      pickUp: req.body.pickUp,
+      dropOff: req.body.dropOff,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      drivingLicence: req.body.drivingLicence,
+      phone: req.body.phone,
+      adress: req.body.adress,
+      passportNumber: req.body.passportNumber,
     })
 
   Reservation.findByIdAndUpdate({_id: req.params.id}, reservation)
@@ -42,8 +50,34 @@ router.get('/edit/:id',(req, res) =>{
       });
 
     console.log('saved')
-    res.redirect('/')
+    res.redirect('/adminReservetion')
+});
+
+router.delete("/deleteReservation/:id", checkAuthenticated, isAdmin,(req, res) => {
+
+  Reservation.findByIdAndRemove(req.params.id, function (err, car) {
+
+      if (err) return res.status(500).send("User error");
+
+      res.status(200).send("deleted");
+
+  });
+  
 });
 
 
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+
+    res.redirect('/login')
+  }
+
+  function isAdmin(req, res, next){
+    if(req.isAuthenticated() && (req.user.email==='starCar@starCar.com')){
+        return next();
+    }
+    res.redirect('/')
+}
 module.exports = router
