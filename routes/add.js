323296ef75018a222042c27371b4a922f95f3789
SHYN1987 <47955936+SHYN1987@users.noreply.github.com>
2021-10-14 20:22:24 +0200

    Update add.js

--- routes/add.js
+++ routes/add.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const router = express.Router();
 const Car = require('../database/carsdata')
+const User = require('../database/loginModel')
+const multer = require('multer');
 
-router.get('/add', (req, res) =>{
-    res.render('index')
+//----------------- Slika ---------------------
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, './uploads/images')
+    },
+
+    filename: function(req, file, cb){
+        cb(null, Date.now() + file.originalname)
+    }
 })
 
-router.post('/add', (req, res) =>{
+const upload = multer({
+    storage:storage,
+    limits:{
+        fieldSize:1024*1024*3
+    }
+})
+
+
+router.get('/add', checkAuthenticated,isAdmin,(req, res) =>{
+    res.render('add')
+    
+})
+
+router.post('/add', upload.single('image'),(req, res) =>{
     Car.create({
+        image: req.file.filename,
         color: req.body.color,
         model: req.body.model,
         year: req.body.year,
@@ -17,5 +41,20 @@ router.post('/add', (req, res) =>{
     console.log("Uspijesno sacuvano")
     res.redirect('/')
 })
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+
+    res.redirect('/login')
+  }
+
+function isAdmin(req, res, next){
+    if(req.isAuthenticated() && (req.user.email==='starCar@starCar.com')){
+        return next();
+    }
+    res.redirect('/')
+}
 
 module.exports = router
