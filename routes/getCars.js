51c5be22ec615e8020fdb5c5d814147c2652046d
SHYN1987 <47955936+SHYN1987@users.noreply.github.com>
2021-10-14 20:22:35 +0200

    Update getCars.js

--- routes/getCars.js
+++ routes/getCars.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const Car = require('../database/carsdata')
+const Reservation = require('../database/reservationdata')
 
-router.get('/', (req,res) =>{
+router.get('/admin', checkAuthenticated, isAdmin,(req,res) =>{
     Car.find(function(err, car){
         res.render('index', {
             car : car
@@ -11,5 +12,34 @@ router.get('/', (req,res) =>{
 
 
 })
+
+router.get('/adminReservetion', checkAuthenticated, isAdmin,(req,res) =>{
+  Car.find(function(err, cars){
+
+    Reservation.find(function(err, reservation){
+      res.render('adminReservetion', {
+       reservation : reservation,
+       cars: cars
+             
+      })
+    })
+  })
+})
+
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+
+    res.redirect('/login')
+  }
+
+  function isAdmin(req, res, next){
+    if(req.isAuthenticated() && (req.user.email==='starCar@starCar.com')){
+        return next();
+    }
+    res.redirect('/')
+}
 
 module.exports = router
